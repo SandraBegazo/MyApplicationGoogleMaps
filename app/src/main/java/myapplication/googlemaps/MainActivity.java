f13edf921e8bf280510737a07276c7b5e5ad9305
Sandra_Begazo <55434828+SandraBegazo@users.noreply.github.com>
2022-04-18 17:57:43 +0200

    Initial commit

---
+++ app/src/main/java/myapplication/googlemaps/MainActivity.java
@@ -0,0 +1,44 @@
+package myapplication.googlemaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button, button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity1.class);
+                startActivity(intent);
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
